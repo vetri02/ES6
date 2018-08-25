05c47c265cdea50e8746ddeb90f253fc90384f35
Vetrichelvan <vetri02@gmail.com>
2018-08-25 17:23:18 +0530

    ESlint More

--- Code Quality with ESLint/bad_code.js
+++ Code Quality with ESLint/bad_code.js
@@ -1,3 +1,6 @@
+/* globals ga twttr */
+/* eslint-disable no-extend-native */
+
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     // resolve('Hi am a Promise')
@@ -80,3 +83,59 @@ Promise.all([postsPromise, streetCarsPromise])
   .then((responses) => {
     console.log(responses);
   });
+
+ga.track();
+twttr.track();
+
+/* eslint-disable */
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value(searchElement, fromIndex) {
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      // 1. Let O be ? ToObject(this value).
+      const o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      const len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      const n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      let k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        // c. Increase k by 1.
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    },
+  });
+}
+/* eslint-enable */
